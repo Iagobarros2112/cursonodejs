1e51f26b910e8c382c80406945da6aa30b451828
Iagobarros2112 <iagobarrosgomes2112@gmail.com>
2025-04-18 17:55:24 -0300

    dando uma ideia basica sobre crud

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ const express = require("express");
 const server = express();
 
 server.get("/hello", (req, res) => {
+    const {nome, idade} = req.query;
     return res.json({
         title : "hello world",
-        message: "oi meu amigo iago"
+        message: `oi  ${nome}  tudo bem? ` ,
+        idade : idade
+    });
+});
+
+server.get("/hello/:nome/:idade", (req, res) => {
+   const  {nome, idade} = req.params;
+
+    return res.json({
+        title : "hello world",
+        message: `oi  ${nome}  tudo bem? ` ,
+        idade: idade
+
     });
 });
 
