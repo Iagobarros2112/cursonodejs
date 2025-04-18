beb0bf1a9dddbab779bd171aad59eb071c1b7812
Iagobarros2112 <iagobarrosgomes2112@gmail.com>
2025-04-18 15:32:32 -0300

    finalizada primeira tentativa de conexao com node

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+//primeira conexao de dados usando json node js
+// 
 const express = require("express");
 
 const server = express();
+
+server.get("/hello", (req, res) => {
+    return res.json({
+        title : "hello world",
+        message: "oi meu amigo iago"
+    });
+});
 
 server.listen(3000);
